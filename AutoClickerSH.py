a77ea2b0efdd7c6716248327ac3aa2d30a0b7b77
Abdulaziz Suria <62330327+Abdulaziz-04@users.noreply.github.com>
2021-03-03 18:08:53 +0530

    Add files via upload

---
+++ AutoClickerSH.py
@@ -0,0 +1,56 @@
+import numpy as np
+import time
+from PIL import Image
+from ppadb.client import Client
+
+# Automating android Game Stick Hero
+
+# Checking devices via ADB
+adb = Client(host='127.0.0.1', port=5037)
+devices = adb.devices()
+if(len(devices) == 0):
+    print('no device attached ! \n')
+    quit()
+device = devices[0]
+# Taking screenshot and storing it as png file
+while True:
+    transitions = []
+    image = device.screencap()
+    with open('screen.png', 'wb') as f:
+        f.write(image)
+    image = Image.open('screen.png')
+    # Converting image to 2d Array
+    image = np.array(image, dtype=np.uint8)
+    # print(image[2000])
+    ignore = True
+    black = True
+    # Getting list of pixels on 200th row,approximated by testing
+    # Removing the 4th value(channel value)
+    pixels = [list(i[:3]) for i in image[2000]]
+    for i, pixel in enumerate(pixels):
+        r, g, b = [int(i) for i in pixel]
+        # Ignoring the initial pixels which may not be black
+        if ignore and r+g+b != 0:
+            continue
+        ignore = False
+        # If black is true but color is not black,we get our starting point
+        if black and r+g+b != 0:
+            black = not black
+            transitions.append(i)
+            continue
+        # Again if we see color but the sum results into black,we have reached the pillar
+        if not black and r+g+b == 0:
+            black = not black
+            transitions.append(i)
+            continue
+    # Getting the positions
+    start, pillar1, pillar2 = transitions
+    # Calculating distances
+    gap = pillar1-start
+    width = pillar2-pillar1
+    # Adjusting to get PERFECT everytime
+    distance = (gap+width/2)*0.98
+    # ADB command to interact with android, here distance and frame time is almost same,so we can use distance
+    device.shell(f'input touchscreen swipe 500 500 500 500 {int(distance)}')
+    # Wait for the next turn
+    time.sleep(2.3)
---
+++ MsgScheduler.py
@@ -0,0 +1,37 @@
+import datetime
+from decouple import config
+from twilio.rest import Client
+from time import sleep
+
+# Setup Twilio Client
+client = Client(config('SID'), config('AUTH'))
+# Get target date and time
+target = input(
+    'Enter your Date and Time seperated by spaces in format YYYY MM DD HH MM : \n')
+year, month, day, hour, minute = map(int, target.split())
+# Formatting and arranging...
+target_date = datetime.datetime(
+    year=year, month=month, day=day, hour=hour, minute=minute)
+# Get the Text Message
+message = input('Enter the text you want to send : \n')
+# Get the input number
+try:
+    receiver = config('TO')
+except:
+    receiver = ""
+    receiver += 'whatsapp:'
+    receiver += input('Enter target whatsapp number(with country code)(Verification required) : \n')
+print("Task Scheduled ... \n")
+while True:
+    # Get current time
+    current_time = datetime.datetime.now()
+    # If Elapse is high,make the program go to sleep...
+    elapse = target_date - current_time
+    if(elapse > datetime.timedelta(minutes=2)):
+        sleep(elapse.total_seconds()-60)
+    if(target_date < current_time):
+        # Sending the message
+        send_message = client.messages.create(
+            body=message, from_=config('FROM'), to=config('TO'))
+        break
+print('Message Sent Succesfully! \n')
